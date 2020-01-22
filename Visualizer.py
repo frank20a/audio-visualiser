1f9dc8bfd022610f84af2c8b090e8e5558706bf8
Frank F <frank1999ipod@gmail.com>
2020-01-22 13:22:57 +0200

    Changed bar height and the way colours are calculated

--- Visualizer.py
+++ Visualizer.py
@@ -11,13 +11,13 @@ green = (0, 255, 0)
 red = (255, 0, 0)
 black = (0, 0, 0)
 r = 255
-g = 1
-b = 0
+g = 75
+b = 76
 
 bar = []
-fpsLimiter = 2  # Dictates that the screen is refreshed 3 times slower
-topDelay = fpsLimiter * 0  # Good idea to make it a multiple of fpsLimiter
-sens = 0.0375  # Limits the amplitude of the spectrum
+fpsLimiter = 2              # Dictates that the screen is refreshed 3 times slower
+topDelay = fpsLimiter * 0   # Good idea to make it a multiple of fpsLimiter
+sens = 0.025                # Limits the amplitude of the spectrum
 btDtct = [False, False]
 fps = 0
 
@@ -27,16 +27,14 @@ class Window:
         self._running = True
         self.screen = None
         self.size = self.weight, self.height = 620, 600
-        self.tops = [[0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green],
-                     [0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green], [0, 0, green],
-                     [0, 0, green], [0, 0, green]]
-        self.color = (r, g, b)
+        self.tops = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
+                     [0, 0], [0, 0]]
         self.counter = 0
 
     def changeColourBtDtct(self):
         global r, g, b
         btDtct[0] = btDtct[1]
-        if (bar[0] + bar[1]) / 2 > 16:
+        if (bar[2] + bar[3]) / 2 > 20:
             btDtct[1] = True
         else:
             btDtct[1] = False
@@ -46,23 +44,36 @@ class Window:
             g = randint(0, 255)
             b = randint(0, 255)
 
-        self.color = (r, g, b)
+        return [(min(max(r, 75), 240), min(max(g, 75), 240), min(max(b, 75), 240)) for i in range(20)]
 
     def fadeColours(self):
         global r, g, b
-        if b <= 0 and g > 0:
-            r = 0
-            r += 10
-            g -= 10
-        if g <= 0 and r > 0:
-            g = 0
-            b += 10
-            r -= 10
-        if r <= 0 and b > 0:
-            r = 0
-            g += 10
-            b -= 10
-        self.color = (min(max(r, 75), 240), min(max(g, 75), 240), min(max(b, 75), 240))
+
+        if self.counter % 2 == 0:
+            if b <= 75 and g > 75:
+                b = 75
+                r += 5
+                g -= 5
+            if g <= 75 and r > 75:
+                g = 75
+                r -= 5
+                b += 5
+            if r <= 75 and b > 75:
+                r = 75
+                g += 5
+                b -= 5
+        return [(min(max(r, 75), 240), min(max(g, 75), 240), min(max(b, 75), 240)) for i in range(20)]
+
+    def peakingColours(self):
+        res = []
+        for j in range(20):
+            if j > 17:
+                res.append(red)
+            elif j > 12:
+                res.append(yellow)
+            else:
+                res.append(green)
+        return res
 
     def on_init(self):
         global fps
@@ -96,33 +107,23 @@ class Window:
         self.screen.fill(black)
         self.screen.blit(pygame.font.SysFont('Arial Bold', 30).render('FPS: %5.2f' % fps, False, red), (10, 10))
 
+        # self.changeColourBtDtct()
+        # self.fadeColours()
+        # self.peakingColours()
+        pallette = self.peakingColours()
+
         for n, i in enumerate(bar):
             if topDelay > 0:
                 if i > self.tops[n][0]:
-                    self.tops[n][0] = min(int(i), 13)
+                    self.tops[n][0] = min(int(i), 20)
                     self.tops[n][1] = 0
-                    if i > 11:
-                        self.tops[n][2] = red
-                    elif i > 7:
-                        self.tops[n][2] = yellow
-                    else:
-                        self.tops[n][2] = green
                 if self.tops[n][1] % topDelay == 0: self.tops[n][0] -= 1
 
-            for j in range(min(int(i) + 1, 14)):
-                if j > 11:
-                    self.color = red
-                elif j > 7:
-                    self.color = yellow
-                else:
-                    self.color = green
+            for j in range(min(int(i) + 1, 20)):
+                pygame.draw.rect(self.screen, pallette[j], pygame.Rect(100 + n * 30, 500 - j * 15, 25, 10))
 
-                self.changeColourBtDtct()
-
-                pygame.draw.rect(self.screen, self.color, pygame.Rect(100 + n * 30, 500 - j * 30, 25, 25))
-
-            if topDelay > 0 and self.tops[n][0] >= 0: pygame.draw.rect(self.screen, self.tops[n][2],
-                        pygame.Rect(100 + n * 30, 500 - (1 + self.tops[n][0]) * 30, 25, 25))
+            if topDelay > 0 and self.tops[n][0] >= 0: pygame.draw.rect(self.screen, red,
+                        pygame.Rect(100 + n * 30, 500 - (1 + self.tops[n][0]) * 15, 25, 10))
             self.tops[n][1] += 1
 
     def on_cleanup():
@@ -143,8 +144,8 @@ class Window:
 A = AudioInput.AudioInput(2048, 96000, 4096, 1)
 
 # get indexes for frequencies
-f = [0, A.indexFromFreq(150), A.indexFromFreq(250), A.indexFromFreq(500), A.indexFromFreq(1250),
-     A.indexFromFreq(2000), A.indexFromFreq(2750), A.indexFromFreq(3500)]
+f = [0, A.indexFromFreq(60), A.indexFromFreq(120), A.indexFromFreq(220), A.indexFromFreq(600),
+     A.indexFromFreq(1000), A.indexFromFreq(2000), A.indexFromFreq(5000)]
 print(f)
 
 
