d73a6b8cba93c109f435c69a393f0adfcc4c98e7
Frank F <frank1999ipod@gmail.com>
2020-01-28 23:02:33 +0200

    Added settings panel + Multi-screen available (Different geometries coming soon) + Screen panels added for SpectrumVisual Screen

--- Visualizer.py
+++ Visualizer.py
@@ -5,7 +5,7 @@ from time import time
 import os
 import tkinter as tk
 from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk
 from pygame.locals import *
 
 
@@ -23,7 +23,8 @@ class Dimension:
 
 
 class Window:
-    def __init__(self, screens: tuple = (), fpsLimiter=1):
+    def __init__(self, audioDevice, screens: tuple = (), fpsLimiter=1):
+        self.audioDevice = audioDevice
 
         self.fpsLimiter = fpsLimiter
         self.screens = screens
@@ -52,7 +53,7 @@ class Window:
 
     def loop(self):
         t = time()
-        A.getData()
+        self.audioDevice.getData()
 
         self.counter += 1
 
@@ -70,20 +71,21 @@ class Window:
     def render(self):
         self.screen.fill(black)
 
+        tempX = 0
         for i in self.screens:
             display = i.render()
             for nx, x in enumerate(display):
                 for ny, y in enumerate(x):
                     pygame.draw.rect(self.screen, y,
-                                     pygame.Rect(nx * (i.pixel.x + i.pxDist),
+                                     pygame.Rect(tempX + nx * (i.pixel.x + i.pxDist),
                                                  i.resolution.y + i.pxDist - (ny + 1) * (i.pixel.y + i.pxDist),
                                                  i.pixel.x,
                                                  i.pixel.y))
+            tempX += i.resolution.x + 10
 
         self.screen.blit(pygame.font.SysFont('Arial Bold', 30).render('FPS: %5.2f' % self.fps, False, red), (10, 10))
 
     def cleanup(self):
-        print("pygame OUT")
         pygame.quit()
 
     def execute(self):
@@ -92,12 +94,15 @@ class Window:
                 self.event(event)
             self.loop()
             self.render()
-        else: self.cleanup()
+        else:
+            self.cleanup()
 
 
 class Screen:
     def __init__(self, size: Dimension, pixel: Dimension, pxDist: int) -> None:
+
         self.updateSize(size, pixel, pxDist)
+        self.settings = None
 
     def render(self):
         raise NotImplementedError()
@@ -111,14 +116,19 @@ class Screen:
 
 
 class SpectrumVisual(Screen):
-    def __init__(self, size: Dimension = Dimension(14, 20), pixel: Dimension = Dimension(25, 10), pxDist: int = 5,
-                 sens: float = 0.035, topDelay: int = 0):
+    number = 0
 
-        self.findex = [0, A.indexFromFreq(30), A.indexFromFreq(60), A.indexFromFreq(90), A.indexFromFreq(120),
-                       A.indexFromFreq(170),
-                       A.indexFromFreq(220), A.indexFromFreq(410), A.indexFromFreq(600), A.indexFromFreq(800),
-                       A.indexFromFreq(1000),
-                       A.indexFromFreq(1500), A.indexFromFreq(2000), A.indexFromFreq(3750), A.indexFromFreq(4500)]
+    def __init__(self, audioDevice: AudioInput, size: Dimension = Dimension(14, 20), pixel: Dimension = Dimension(25, 10), pxDist: int = 5,
+                 sens: float = 0.03, topDelay: int = 0):
+        self.name = self.__class__.__name__ + ' - ' + str(SpectrumVisual.number)
+        SpectrumVisual.number += 1
+
+        self.audioDevice = audioDevice
+        self.findex = [0, self.audioDevice.indexFromFreq(30), self.audioDevice.indexFromFreq(60), self.audioDevice.indexFromFreq(90), self.audioDevice.indexFromFreq(120),
+                       self.audioDevice.indexFromFreq(170),
+                       self.audioDevice.indexFromFreq(220), self.audioDevice.indexFromFreq(410), self.audioDevice.indexFromFreq(600), self.audioDevice.indexFromFreq(800),
+                       self.audioDevice.indexFromFreq(1000),
+                       self.audioDevice.indexFromFreq(1500), self.audioDevice.indexFromFreq(2000), self.audioDevice.indexFromFreq(3750), self.audioDevice.indexFromFreq(4500)]
 
         self.sens = sens
         self.topDelay = topDelay
@@ -129,7 +139,7 @@ class SpectrumVisual(Screen):
         self.counter = 0
 
         # Colour Crossfade Variables
-        self.crossfadeSpeed = 2
+        self.crossfadeSpeed = 8
         self.r = 255
         self.g = 75
         self.b = 76
@@ -140,14 +150,147 @@ class SpectrumVisual(Screen):
         self.beatDetectionBar = 3
         self.beatDetect = [0 for i in range(20)]
 
+        # Create settings menu for display
+        self.settings = self.Menu(self)
+
+    class Menu(tk.Frame):
+        def __init__(self, parent):
+            self.parent = parent
+
+        def create(self, *args, **kwargs):
+            tk.Frame.__init__(self, width=200, height=100, *args, **kwargs)
+            self.grid(column=0, row=0)
+
+            tk.Label(self, text=self.parent.name).grid(row=0, column=0, columnspan=4)
+
+            # Colour Mode Settings
+            tk.Label(self, text="Change Colour Mode").grid(row=1, column=0)
+            self.colourCombo = ttk.Combobox(self, values=["peaking", "beat", "gradient", "cross", "gradientCross",
+                                                          "gradientBeat"])
+            self.colourCombo.grid(row=1, column=1, sticky=W)
+            self.colourCombo.current(5)
+            self.colourCombo.bind("<<ComboboxSelected>>", self.ColourModeSelected)
+            self.colour1 = tk.Text(self, width=6, height=1)
+            self.colour1.insert(tk.END, "000000")
+            self.colour1.bind("<KeyRelease>", self.ColourModeSelected)
+            self.colour1.grid(row=1, column=2)
+            self.colour2 = tk.Text(self, width=6, height=1)
+            self.colour2.insert(tk.END, "000000")
+            self.colour2.bind("<KeyRelease>", self.ColourModeSelected)
+            self.colour2.grid(row=1, column=3)
+
+            # Delay Settings
+            self.delayVar = IntVar()
+            self.delayCheck = tk.Checkbutton(self, text="Delay Tops", variable=self.delayVar, command=self.changeDelay)
+            self.delayCheck.grid(row=2, column=0, columnspan=2, sticky=W)
+            self.delayValText = tk.Text(self, width=2, height=1)
+            self.delayValText.bind("<KeyRelease>", self.changeDelay)
+            self.delayValText.grid(row=2, column=1, sticky=W)
+            self.delayValText.insert(tk.END, str(self.parent.topDelay))
+            if self.parent.topDelay > 0: self.delayCheck.invoke()
+
+            # Sensitivity Settings
+            self.sensitivityLabel = tk.Label(self, text="Sensitivity")
+            self.sensitivityLabel.grid(row=3, column=0, sticky=SW)
+            self.sensitivitySlider = tk.Scale(self, command=self.changeSensitivity, orient=HORIZONTAL, from_=0.001,
+                                              to=0.5, resolution=0.001, showvalue=0, length=350,
+                                              sliderlength=15)
+            self.sensitivitySlider.set(self.parent.sens)
+            self.sensitivitySlider.grid(row=3, column=1, columnspan=3, sticky=W)
+            self.changeSensitivity()
+
+            # Beat Detection Sensitivity Settings
+            self.beatSensitivityLabel = tk.Label(self, text="Beat Sensitivity")
+            self.beatSensitivityLabel.grid(row=4, column=0, sticky=SW)
+            self.beatSensitivitySlider = tk.Scale(self, command=self.changeBeatSensitivity, orient=HORIZONTAL,
+                                                  from_=1.5,
+                                                  to=3, resolution=0.15, showvalue=0, length=350,
+                                                  sliderlength=15)
+            self.beatSensitivitySlider.set(self.parent.beatDetectSensitivity)
+            self.beatSensitivitySlider.grid(row=4, column=1, columnspan=3, sticky=W)
+            self.changeBeatSensitivity()
+
+            # Beat Detection Bar Settings
+            self.beatBarLabel = tk.Label(self, text="Detection Bar")
+            self.beatBarLabel.grid(row=5, column=0, sticky=SW)
+            self.beatBarSlider = tk.Scale(self, command=self.changeDetectionBar, orient=HORIZONTAL, from_=1,
+                                          to=self.parent.size.x,
+                                          resolution=1, showvalue=0, length=130, sliderlength=15)
+            self.beatBarSlider.set(self.parent.beatDetectionBar)
+            self.beatBarSlider.grid(row=5, column=1, sticky=W)
+            self.changeDetectionBar()
+
+            # Beat Detection Threshold Settings
+            self.beatThreshLabel = tk.Label(self, text="Beat Threshold")
+            self.beatThreshLabel.grid(row=5, column=2, sticky=SW)
+            self.beatThreshSlider = tk.Scale(self, command=self.changeBeatThresh, orient=HORIZONTAL, from_=1,
+                                             to=int(self.parent.size.y * 1.5),
+                                             resolution=1, showvalue=0, length=100, sliderlength=15)
+            self.beatThreshSlider.set(self.parent.beatDetectThreshold)
+            self.beatThreshSlider.grid(row=5, column=3, sticky=W)
+            self.changeDetectionBar()
+
+            # Colour Crossfade Sensitivity Settings
+            self.fadeSpeedLabel = tk.Label(self, text="Colour Fade Speed")
+            self.fadeSpeedLabel.grid(row=6, column=0, sticky=SW)
+            self.fadeSpeedSlider = tk.Scale(self, command=self.changeFadeSpeed, orient=HORIZONTAL, from_=15,
+                                            to=1, resolution=1, showvalue=0, length=350, sliderlength=15)
+            self.fadeSpeedSlider.set(self.parent.crossfadeSpeed)
+            self.fadeSpeedSlider.grid(row=6, column=1, columnspan=3, sticky=W)
+            self.changeFadeSpeed()
+
+        def changeFadeSpeed(self, event=None):
+            self.parent.crossfadeSpeed = int(self.fadeSpeedSlider.get())
+            self.fadeSpeedLabel.config(text="Colour Fade Speed: {0}".format(int(self.fadeSpeedSlider.get())))
+
+        def changeBeatThresh(self, event=None):
+            self.parent.beatDetectThreshold = int(self.beatThreshSlider.get()) - 1
+            self.beatThreshLabel.config(text="Beat Threshold: {0:2}".format(int(self.beatThreshSlider.get())))
+
+        def changeDetectionBar(self, event=None):
+            self.parent.beatDetectionBar = int(self.beatBarSlider.get()) - 1
+            self.beatBarLabel.config(text="Detection Bar: {0}".format(int(self.beatBarSlider.get())))
+
+        def changeBeatSensitivity(self, event=None):
+            self.parent.beatDetectSensitivity = self.beatSensitivitySlider.get()
+            self.beatSensitivityLabel.config(text="Beat Sensitivity: {0:3.2f}".format(self.beatSensitivitySlider.get()))
+
+        def changeSensitivity(self, event=None):
+            self.parent.sens = self.sensitivitySlider.get()
+            self.sensitivityLabel.config(text="Sensitivity: {0:4.3f}".format(self.sensitivitySlider.get()))
+
+        def changeDelay(self, event=None):
+            if self.delayVar.get():
+                t = int(self.delayValText.get("1.0", "end-1c"))
+                if t > 0:
+                    self.parent.topDelay = t
+                else:
+                    self.delayValText.delete('1.0', tk.END)
+                    self.delayValText.insert(tk.END, "1")
+                    self.parent.topDelay = 1
+
+
+            else:
+                self.parent.topDelay = 0
+
+        def ColourModeSelected(self, event):
+            try:
+                c1 = self.colour1.get("1.0", "end-1c")
+                c1 = (int(c1[0:2], 16), int(c1[2:4], 16), int(c1[4:6], 16))
+                c2 = self.colour2.get("1.0", "end-1c")
+                c2 = (int(c2[0:2], 16), int(c2[2:4], 16), int(c2[4:6], 16))
+                self.parent.changePalette(self.colourCombo.get(), c1, c2)
+            except:
+                pass
+
     def calcBars(self):
         self.findex.sort()
         self.bar = []
         for i in range(len(self.findex) - 1):
-            self.bar.append(self.sens * A.getSpectralBar(self.findex[i], self.findex[i + 1]))
+            self.bar.append(self.sens * self.audioDevice.getSpectralBar(self.findex[i], self.findex[i + 1]))
 
     def addBand(self, x=0):
-        if x > 0: self.findex.append(A.indexFromFreq(x))
+        if x > 0: self.findex.append(self.audioDevice.indexFromFreq(x))
         self.tops = [[0, 0] for i in range((len(self.findex) - 1) * 2)]
         self.updateSize(size=Dimension(len(self.findex) - 1, self.size.y))
 
@@ -172,16 +315,16 @@ class SpectrumVisual(Screen):
         if self.counter % self.crossfadeSpeed == 0:
             if self.b <= 75 and self.g > 75:
                 self.b = 75
-                self.r += 5
-                self.g -= 5
+                self.r += 20
+                self.g -= 20
             if self.g <= 75 and self.r > 75:
                 self.g = 75
-                self.r -= 5
-                self.b += 5
+                self.r -= 20
+                self.b += 20
             if self.r <= 75 and self.b > 75:
                 self.r = 75
-                self.g += 5
-                self.b -= 5
+                self.g += 20
+                self.b -= 20
         return [(min(max(self.r, 75), 240), min(max(self.g, 75), 240), min(max(self.b, 75), 240)) for i in
                 range(self.size.y)]
 
@@ -231,8 +374,14 @@ class SpectrumVisual(Screen):
         elif x == "gradient":
             self.palette = self.gradient(args[0], args[1])
         elif x == "cross":
+            self.r = 255
+            self.g = 75
+            self.b = 76
             self.palette = self.crossfadeColours
         elif x == "gradientCross":
+            self.r = 255
+            self.g = 75
+            self.b = 76
             self.palette = self.gradientCrossfadeColours
         elif x == "gradientBeat":
             self.palette = self.beatDetectGradient
@@ -270,26 +419,47 @@ class SpectrumVisual(Screen):
         return res
 
 
-class Application:
-    def __init__(self, window: Window):
-        self.root = tk.Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.quit)
+class Application(tk.Tk):
+    def __init__(self, window: Window, *args, **kwargs):
+        # Initiating tkinter application
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.protocol("WM_DELETE_WINDOW", self.quit)
 
+        # Integrating pygame window
         self.window = window
-        self.frame = tk.Frame(self.root, width=self.window.width, height=self.window.height)
-        self.frame.pack(side=LEFT)
+        self.frame = tk.Frame(self, width=self.window.width, height=self.window.height)
+        self.frame.grid(row=0, column=0, rowspan=10)
         os.environ['SDL_WINDOWID'] = str(self.frame.winfo_id())
         os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+        # Settings Panel
+        settingSelector = tk.Frame(self)
+        tk.Label(settingSelector, text="Settings for screen: ").pack(side=LEFT)
+        self.settingsSelectorCombo = ttk.Combobox(settingSelector, values=[i.name for i in self.window.screens],
+                                                  width=30)
+        self.settingsSelectorCombo.bind("<<ComboboxSelected>>", self.selectSettingPanel)
+        self.settingsSelectorCombo.current(0)
+        self.settingsSelectorCombo.pack(side=LEFT)
+        settingSelector.grid(row=0, column=1, sticky=NW)
+        self.SettingsContainer = tk.Frame(self)
+        self.SettingsContainer.grid(row=1, column=1, sticky=N)
+        # Create all the Frames for the settings panels
+        for n, i in enumerate(self.window.screens):
+            i.settings.create(self.SettingsContainer)
+        self.selectSettingPanel(None)
+
+    def selectSettingPanel(self, event):
+        self.window.screens[self.settingsSelectorCombo.current()].settings.tkraise()
 
     def open(self):
         self.window.init()
         while True:
-            self.root.update()
+            self.update()
             self.window.execute()
 
     def quit(self):
         self.window._running = False
-        self.root.destroy()
+        self.destroy()
 
 
 purple = (128, 0, 128)
@@ -301,7 +471,7 @@ cyan = (64, 224, 208)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-A = AudioInput.AudioInput(4096, 96000, 4096, 1)
-
-app = Application(Window(screens=(SpectrumVisual(topDelay=0),), fpsLimiter=1))
-app.open()
+if __name__ == "__main__":
+    A = AudioInput.AudioInput(4096, 96000, 4096, 1)
+    app = Application(Window(A, screens=(SpectrumVisual(A, topDelay=0), SpectrumVisual(A, topDelay=2)), fpsLimiter=1))
+    app.open()
